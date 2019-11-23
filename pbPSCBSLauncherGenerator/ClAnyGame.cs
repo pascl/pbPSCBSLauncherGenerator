78735d070fb43262e80de3b2004cfe371c8ca178
pascl <pascl1983@gmail.com>
2019-11-23 22:45:26 +0100

    Add Players field in game class

--- pbPSCBSLauncherGenerator/ClAnyGame.cs
+++ pbPSCBSLauncherGenerator/ClAnyGame.cs
@@ -12,18 +12,35 @@ namespace pbPSCBSLauncherGenerator
         private String _date;
         private String _image;
         private String _publisher;
+        private int _players;
 
-        public ClAnyGame(String title, String date, String image, String publisher)
+        public ClAnyGame(String title, String date, String image, String publisher, int players)
         {
             _title = title;
             _date = date;
             _image = image;
             _publisher = publisher;
+            _players = setPlayers(players);
+        }
+
+        int setPlayers(int val)
+        {
+            int newval = 0;
+            if (val > 0)
+            {
+                newval = val;
+            }
+            else
+            {
+                newval = 1;
+            }
+            return newval;
         }
 
         public String Title { get => _title; set => _title = value; }
         public String Date { get => _date; set => _date = value; }
         public String Image { get => _image; set => _image = value; }
         public String Publisher { get => _publisher; set => _publisher = value; }
+        public int Players { get => _players; set => _players = setPlayers(value); }
     }
 }
